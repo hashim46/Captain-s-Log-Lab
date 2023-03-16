6ebf50f9b808f275c61e8fbbe4271e77cd2c4754
Hashim Siddiqui <hashimedt@gmail.com>
2023-03-16 14:44:30 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+const express = require('express')
+
+// Setup our Express app
+const app = express()
+
+const PORT = 8080 
+
+const connectDB = require('./config/db')
+
+connectDB()
+
+const methodOverride = require('method-override')
+
+const logRoutes = require('./routes/logRoutes')
+
+const { createEngine } = require('jsx-view-engine')
+
+app.set('view engine', 'jsx')
+
+// Create the engine and accept files ending in jsx
+app.engine('jsx', createEngine())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
+
+app.use('/logs', logRoutes)
+
+// app.get('/create', (req, res) => {
+//     console.log(req.body)
+//     res.send(req.body)
+// })
+
+app.listen(PORT, () => {
+    console.log('Listening to the port: ' + PORT)
+})
